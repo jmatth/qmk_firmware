affe5ab4bab2c5e4390294023188cf60961ddae0
Josh Matthews <joshua.matthews@mongodb.com>
2017-01-22 21:34:12 -0500

    Fixing rules for phantom

--- keyboards/phantom/rules.mk
+++ keyboards/phantom/rules.mk
@@ -49,7 +49,8 @@ OPT_DEFS += -DINTERRUPT_CONTROL_ENDPOINT
 #   Atmel DFU loader 4096
 #   LUFA bootloader  4096
 #   USBaspLoader     2048
-OPT_DEFS += -DBOOTLOADER_SIZE=4096
+# OPT_DEFS += -DBOOTLOADER_SIZE=4096
+OPT_DEFS += -DBOOTLOADER_SIZE=512
 
 
 # Build Options
@@ -58,7 +59,7 @@ OPT_DEFS += -DBOOTLOADER_SIZE=4096
 BOOTMAGIC_ENABLE ?= yes	# Virtual DIP switch configuration(+1000)
 MOUSEKEY_ENABLE ?= yes	# Mouse keys(+4700)
 EXTRAKEY_ENABLE ?= yes	# Audio control and System control(+450)
-CONSOLE_ENABLE ?= yes	# Console for debug(+400)
+CONSOLE_ENABLE ?= no	# Console for debug(+400)
 COMMAND_ENABLE ?= yes    # Commands for debug and configuration
 NKRO_ENABLE ?= no		# USB Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
 RGBLIGHT_ENABLE ?= no   # Enable keyboard underlight functionality (+4870)
